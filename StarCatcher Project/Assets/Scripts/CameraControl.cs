5a5cf870bda84dcfac473c183e93dd5c4739a5f8
Julia Berrio <berrioja@gmail.com>
2016-09-12 13:34:55 -0600

    Create Proj.
    Adding Project & Assets

---
+++ StarCatcher Project/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+	public float speed = 40f;
+	private Rigidbody rb;
+
+	void Update () 
+	{
+		rb = GetComponent<Rigidbody>();
+		Vector3 movement = new Vector3 (1f, 0.0f, 0f);
+		rb.AddForce (movement * speed);
+	}
+}
---
+++ StarCatcher Project/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour 
+{
+	//set speed 
+	public float speed = 10f;
+	//CC component
+	private CharacterController controller;
+	//temp Vect3 to move character
+	private Vector3 tempPosition;
+	public float gravity = 3f;
+	public float jumpSpeed = 30f;
+	int AlowedJumps = 1; 
+	int JumpCounter = 0;
+
+	void Start () 
+	{
+		//to 'find' the cc
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update()
+	{
+		//input to be able to jump
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			if (controller.isGrounded) 
+			{
+				tempPosition.y = jumpSpeed;
+				JumpCounter = 0;
+			}
+
+			if (!controller.isGrounded && JumpCounter < AlowedJumps) 
+			{
+				tempPosition.y = jumpSpeed;
+				JumpCounter++;
+			}
+		}
+
+		//add gravity
+		tempPosition.y -= gravity;
+
+		//the actual input from keyboard to character
+		tempPosition.x = speed * Input.GetAxis ("Horizontal");
+		//to access controller & make it move character in seconds/not frames
+		controller.Move (tempPosition * Time.deltaTime);
+
+	}
+}
