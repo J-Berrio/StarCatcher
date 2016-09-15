38f51d2dac3e51c64e61360384f643acc9aadba3
Julia Berrio <berrioja@gmail.com>
2016-09-15 09:25:27 -0600

    Working on Func/Methods

--- StarCatcherProject/Assets/Scripts/CameraControl.cs
+++ StarCatcherProject/Assets/Scripts/CameraControl.cs
@@ -6,10 +6,20 @@ public class CameraControl : MonoBehaviour
 	public float speed = 40f;
 	private Rigidbody rb;
 
-	void Update () 
+	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () 
+	{
+		MoveCamera ();	
+	}
+
+	void MoveCamera()
+	{
 		Vector3 movement = new Vector3 (1f, 0.0f, 0f);
-		rb.AddForce (movement * speed);
+		rb.AddForce (movement * speed * Time.deltaTime);
 	}
 }
+
