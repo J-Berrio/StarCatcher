683e577713c066021f92b50be7d5cc269f7f93ef
Julia Berrio <berrioja@gmail.com>
2016-09-16 17:01:45 -0600

    Delegates/SwitchStatements

---
+++ StarCatcherProject/Assets/Scripts/IfSwitchStatements.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfSwitchStatements : MonoBehaviour 
+{
+	public int jediPassword = 3;
+
+	
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			switch (jediPassword) 
+			{
+			case 3:
+				print ("Welcome to the Jedi Temple. Please, enter the keypass.");
+				break;
+
+			case 2:
+				print ("Incorrect. Please enter the keypass");
+				break;
+
+			case 1:
+				print ("You have one more attempt.");
+				break;
+
+			default:
+				print ("You have reached the maximum attempts to enter the keypass. Calling clone troopers for extermination now.");
+				break;
+			}
+		}
+	}
+		
+}
