eadadd417639ec7cc63ece09a5d55ad0aa0f44b1
Julia Berrio <berrioja@gmail.com>
2016-09-19 12:24:54 -0600

    Loops

---
+++ StarCatcherProject/Assets/Scripts/Loops.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour 
+{
+	void Start()
+	{
+		EquiptmentCheck ();
+	}
+
+	void EquiptmentCheck()
+	{
+		string[] strings = new string[5];
+
+		strings[0] = "Lightsaber";
+		strings[1] = "ComLink";
+		strings[2] = "Breathing Device";
+		strings[3] = "Holocron";
+		strings[4] = "Training Remote";
+
+		foreach(string item in strings)
+		{
+			print(item);
+		}
+
+	}
+}
+
+
