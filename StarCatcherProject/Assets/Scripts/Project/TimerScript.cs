3ff33efb7460f22028112c6486401beb19e5211b
Julia Berrio <berrioja@gmail.com>
2016-10-27 18:47:01 -0600

    Countdown Stops and goes to Menu

--- StarCatcherProject/Assets/Scripts/Project/TimerScript.cs
+++ StarCatcherProject/Assets/Scripts/Project/TimerScript.cs
@@ -1,22 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerScript : MonoBehaviour 
 {
 	public Text timerText;
-	public float myTimer = 100;
+	public float myTimer = 100; 
+	private bool gameOver = false;
+	public Text gameOverText;
+	private bool timerIsActive = true;
 
 	void Start()
 	{
 		timerText = GetComponent<Text> ();
+
+		gameOverText.text = "";
 	}
 
 	void Update()
 	{
-		myTimer -= Time.deltaTime;
-		timerText.text = "Time Left: " + myTimer.ToString ("f0");
+		if (timerIsActive)
+		{
+			myTimer -= Time.deltaTime;
+			timerText.text = "Time Left: " + myTimer.ToString ("f0");
+			if (myTimer < 0) {
+				myTimer = 0;
+				timerIsActive = false;
+				GameOver ();
+			}
 
+		}
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+
+		SceneManager.LoadScene (0);
+		
 	}
 
 }
