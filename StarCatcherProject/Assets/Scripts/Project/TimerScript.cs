1a2467aa33f271c5d06fa1ffa91edb3a2828c7d9
Julia Berrio <berrioja@gmail.com>
2016-10-27 18:16:43 -0600

    Countdown Timer (seconds)

--- StarCatcherProject/Assets/Scripts/Project/TimerScript.cs
+++ StarCatcherProject/Assets/Scripts/Project/TimerScript.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class TimerScript : MonoBehaviour {
+public class TimerScript : MonoBehaviour 
+{
+	public Text timerText;
+	public float myTimer = 100;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		timerText = GetComponent<Text> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update()
+	{
+		myTimer -= Time.deltaTime;
+		timerText.text = "Time Left: " + myTimer.ToString ("f0");
+
 	}
+
 }
