54abb8f60ff0a1a6ee7563c0accafd99772ac30b
Julia Berrio <berrioja@gmail.com>
2016-11-10 13:09:48 -0700

    Working on AI

---
+++ StarCatcherProject/Assets/Scripts/Project/Setup/NavStuff.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavStuff : MonoBehaviour 
+{
+	public NavMeshAgent myAgent;
+	public Transform player;
+
+	void Start()
+	{
+		myAgent.GetComponent<NavMeshAgent> ();
+	}
+
+	void Update()
+	{
+		myAgent.destination = player.position;
+	}
+}
---
+++ StarCatcherProject/Assets/WolfStarCollect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WolfStarCollect : MonoBehaviour 
+{
+	private int count;
+	public Text countText;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+}
