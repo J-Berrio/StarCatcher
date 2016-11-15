e9db4ffdaf8675f3b1e5976d87aa23ffaaf639fa
Julia Berrio <berrioja@gmail.com>
2016-11-15 14:08:28 -0700

    AI fixes and Bunny Jump

---
+++ StarCatcherProject/Assets/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour 
+{
+	public Transform goal;
+	public float time = 5;
+
+	void Start()
+	{
+		StartCoroutine (Spawner ());
+	}
+
+	IEnumerator Spawner()
+	{
+		yield return new WaitForSeconds (time);
+		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+		agent.destination = goal.position;
+	}
+
+}
