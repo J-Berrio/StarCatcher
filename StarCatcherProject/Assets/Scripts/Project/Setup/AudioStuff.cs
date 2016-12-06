332dcb37bda3d829e25f0575970851ec14cc6417
Julia Berrio <berrioja@gmail.com>
2016-12-06 13:07:56 -0700

    Audio Fix

---
+++ StarCatcherProject/Assets/Scripts/Project/Setup/AudioStuff.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioStuff : MonoBehaviour {
+
+	public bool playOnAwake;
+
+	void Awake()
+	{
+		DontDestroyOnLoad (transform.gameObject);
+	}
+}
